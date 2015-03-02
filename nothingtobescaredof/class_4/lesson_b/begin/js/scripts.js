4e5c7288a80739ee7ab807ecb6d8ba5d9ec561d4
unknown <Ericf@LT-X1-NYC-091.ny.rga.com>
2015-03-02 13:55:02 -0500

    updating class 4 files with comments/feedback

--- nothingtobescaredof/class_4/lesson_b/begin/js/scripts.js
+++ nothingtobescaredof/class_4/lesson_b/begin/js/scripts.js
@@ -1,6 +1,8 @@
 // *** First follow Step 1 in both index.html and styles.css ***
 
 // VARIABLES 
+// Variables are like boxes
+// http://finomenon.com/stuff/teaching/nothingtobescaredof/images/variables.png
 // Step 1: 	Create a variable called "message" and set it equal to "Hello, World!"
 // 		   	Replace console.log arugument with the message variable "console.log(message)"
 // 			Run in browser. Open the console, and see the output has not changed
@@ -9,6 +11,22 @@
 
 
 // FUNCTIONS
+// Functions are like recipes
+// 
+// to make spaghetti you:
+// 1) boil salted water
+// 2) throw in the spaghetti
+// 3) check repeatedly if spaghetti are ready
+// 4) take out the spaghetti
+// 
+// in pseudocode you:
+// function make_spaghetti() {
+//     boil_salted_water();
+//     while(notReady == true) {
+//         wait(30);
+//     }
+//     take_out();
+
 // Step 3: 	Create a function called "toggleEmailForm()"
 // 			inside the function, write console.log(open);
 // 			Follow step 2 in index.html
@@ -16,6 +34,7 @@
 
 
 // CONDITIONALS
+// Conditionals are you decision makers 
 // Step 4: 	Create an if statement to check if open == true
 // 			Move the console.log statment inside the if conditional 
 // 			Load in the browser and open the console. Click the "Send Lawyer Dog and Email" button and observe the output
@@ -33,12 +52,18 @@
 
 
 // LOOPS
+// Loops let you repeat the same thing many times.
 // Step 5: 	Inside the addSomeRandomSquares() function, create a for loop that repeats 10 times	
 // 			Load in browser, and click the red square
 
+// Some practical examples of JS:
+// - Slideout menus
+// - Modal Windows
+// - Search Suggest
+// - Google analytics / click tracking
+
 
 console.log('Hello, World!');
-
 
 
 function addSomeRandomSquares() {
